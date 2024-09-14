9938f656b6ad61b20ee7c9b9681f47258665a6c2
arvin <rwiiinzakeri@gmail.com>
2024-09-14 14:36:48 +0330

    header done

--- scripts/index.js
+++ scripts/index.js
@@ -5,26 +5,38 @@ const closeSidebar = document.querySelector("closeSidebar");
 const sidebarClose = document.getElementById("sidebarClose");
 const blurDiv = document.getElementById("blurDiv");
 sidebarBtn.addEventListener("click", () => {
-  sidebar.classList.replace("-right-full", "right-0");
+  sidebar.classList.replace("-right-full", "-right-4");
   blurDiv.classList.remove("hidden");
 });
 sidebarClose.addEventListener("click", () => {
-  sidebar.classList.replace("right-0", "-right-full");
+  sidebar.classList.replace("-right-4", "-right-full");
   blurDiv.classList.add("hidden");
 });
 blurDiv.addEventListener("click", () => {
-  console.log("object");
   blurDiv.classList.add("hidden");
-  sidebar.classList.replace("right-0", "-right-full");
-  tutorialsSidebar.classList.replace("right-0", "-right-full");
+  sidebar.classList.replace("-right-4", "-right-full");
+  tutorialsSidebar.classList.replace("-right-4", "-right-full");
 });
 // tutorials side bar
 const tutorialsBtn = document.getElementById("tutorialsBtn");
 const tutorialsSidebar = document.getElementById("tutorialsSidebar");
 const tutorialsCloseBtn = document.getElementById("tutorialsCloseBtn");
+const tutorialsSidebarBtn = document.getElementById("tutorialsSidebarBtn");
 tutorialsBtn.addEventListener("click", () => {
-  tutorialsSidebar.classList.replace("-right-full", "right-0");
+  tutorialsSidebar.classList.replace("-right-full", "-right-4");
 });
-tutorialsSidebar.addEventListener("click", () => {
-  tutorialsSidebar.classList.replace("right-0", "-right-full");
+tutorialsSidebarBtn.addEventListener("click", () => {
+  tutorialsSidebar.classList.replace("-right-4", "-right-full");
+});
+// tutorial side bar desktop
+const tutorialSidebar = document.getElementById("tutorialSidebar");
+const arrowIcon = document.getElementById("arrowIcon");
+arrowIcon.addEventListener("click", () => {
+  if (arrowIcon.children[2].classList.contains("rotate-0")) {
+    arrowIcon.children[2].classList.replace("rotate-0", "rotate-180");
+    tutorialSidebar.classList.replace("hidden", "block");
+  } else {
+    arrowIcon.children[2].classList.replace("rotate-180", "rotate-0");
+    tutorialSidebar.classList.replace("block", "hidden");
+  }
 });
